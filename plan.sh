6eb6f4341c5b98a0bce425bd9703c0ec3df1353f
Dariusz Krol <dkrol@agh.edu.pl>
2015-05-06 13:51:40 -0700

    Better synthethic workflow support

--- plan.sh
+++ plan.sh
@@ -3,12 +3,39 @@
 SITE=hopper
 OUTPUT_SITE=hopper
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 WORKFLOW_DIR"
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 WORKFLOW_DIR CONFIG_FILE <--synthetic|--hydrogen>"
     exit 1
 fi
 
 WORKFLOW_DIR=$1
+CONFIG_FILE=$2
+
+if [ ! -e "$CONFIG_FILE" ]; then
+    echo "No such file: $CONFIG_FILE"
+    exit 1
+fi
+
+SYNTHETIC=false
+if [ "$3" == "--synthetic" ]; then
+
+    SYNTHETIC=true
+
+    echo "python daxgen.py --synthetic $CONFIG_FILE $WORKFLOW_DIR"
+    python daxgen.py --synthetic $CONFIG_FILE $WORKFLOW_DIR
+else
+    if [ "$3" == "--hydrogen" ]; then
+
+        echo "python daxgenQ.py $CONFIG_FILE $WORKFLOW_DIR"
+        python daxgenQ.py $CONFIG_FILE $WORKFLOW_DIR
+
+    else
+
+        echo "python daxgen.py $CONFIG_FILE $WORKFLOW_DIR"
+        python daxgen.py $CONFIG_FILE $WORKFLOW_DIR
+
+    fi
+fi
 
 if [ -d "$WORKFLOW_DIR" ]; then
     WORKFLOW_DIR=$(cd $WORKFLOW_DIR && pwd)
@@ -22,15 +49,22 @@ INPUT_DIR=$DIR/inputs
 SUBMIT_DIR=$WORKFLOW_DIR/submit
 DAX=$WORKFLOW_DIR/dax.xml
 TC=$DIR/tc.txt
+
+if [ $SYNTHETIC = true ]; then
+    TC=$DIR/tc-fake.txt
+fi
+
+echo "We will us '$TC' as transformation catalog"
+
 RC=$WORKFLOW_DIR/rc.txt
 SC=$DIR/sites.xml
 PP=$DIR/pegasus.properties
 
 echo "Planning workflow..."
 pegasus-plan \
-    -Dpegasus.catalog.site.file=$SC \
     -Dpegasus.catalog.replica=File \
     -Dpegasus.catalog.replica.file=$RC \
+    -Dpegasus.catalog.transformation=Text \
     -Dpegasus.catalog.transformation.file=$TC \
     --conf $PP \
     --dax $DAX \
@@ -38,5 +72,5 @@ pegasus-plan \
     --input-dir $INPUT_DIR \
     --sites $SITE \
     --output-site $OUTPUT_SITE \
+    --submit \
     --cleanup leaf
-
